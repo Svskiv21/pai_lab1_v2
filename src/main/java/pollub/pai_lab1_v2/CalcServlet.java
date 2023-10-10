2d23a7fdc41b5d8c47d454961a90bfd294f88190
Natalia Krukar <natalia.krukar@gmail.com>
2023-10-10 11:34:49 +0200

    working

--- src/main/java/pollub/pai_lab1_v2/CalcServlet.java
+++ src/main/java/pollub/pai_lab1_v2/CalcServlet.java
@@ -15,62 +15,74 @@ public class CalcServlet extends HttpServlet {
     public void init(){
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-
-        String num1 = req.getParameter("num1");
-        String num2 = req.getParameter("num2");
-        String operator = req.getParameter("operator");
-//        oblicz(req);
-
-        PrintWriter out = resp.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Wynik obliczeń:</h1>");
-        out.println("<p>" + num1 + " " + operator + " " + num2 + " = " + oblicz(req) + "</p>");
-        out.println("</body></html>");
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-
-        String num1 = req.getParameter("num1");
-        String num2 = req.getParameter("num2");
-        String operator = req.getParameter("operator");
-//        oblicz(req);
-
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Wynik obliczeń:</h1>");
-        out.println("<p>" + num1 + " " + operator + " " + num2 + " = " + oblicz(req) + "</p>");
-        out.println("</body></html>");
-        out.close();
-    }
-
-    public Float oblicz(HttpServletRequest request){
-        Float num1 = Float.parseFloat(request.getParameter("num1"));
-        Float num2 = Float.parseFloat(request.getParameter("num2"));
-        String operator = request.getParameter("operator");
-
-        switch (operator) {
-            case "+":
-                return num1 + num2;
-            case "-":
-                return num1 - num2;
-            case "*":
-                return num1 * num2;
-            default:
-                // mam wrażenie że nie powinno być to na defaulcie + nie ma ustawionego
-                // defaulta w formularzu wiec sie wywali
-                return num1 / num2;
+     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HelloServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Wynik obliczeń " + oblicz(request) + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
+    }
+
+    public String oblicz(HttpServletRequest request){
+        String komunikat = "";
+        try {
+            Double num1 = Double.parseDouble(request.getParameter("num1"));
+            Double num2 = Double.parseDouble(request.getParameter("num2"));
+            String operator = request.getParameter("operator");
+            boolean isCorrect = true;
+            Double result = null;
+
+
+            switch (operator) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                default:
+                    if (num1 == 0 || num2 == 0) {
+                        isCorrect = false;
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+            }
+            if (isCorrect) {
+                komunikat = "<p>" + num1 + operator + num2 + " = " + result + "</p>";
+            } else {
+                komunikat = "<p>Nie można dzielić przez 0!</p>";
+            }
+        } catch (NumberFormatException ex) {
+            komunikat = "<p>Niepoprawne dane!</p>";
+        }
+        return komunikat;
     }
 
     @Override
     public void destroy() {
         super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
     }
 }
